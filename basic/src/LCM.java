d6966e058cc29510d11dbe0f4f2de74c250b09a7
prachixmishrax <147589763+prachixmishrax@users.noreply.github.com>
2026-06-21 00:22:53 +0530

    prime number from 2 to n

--- basic/src/LCM.java
+++ basic/src/LCM.java
@@ -1,19 +1,1 @@
-import java.util.Scanner;
 
-public class LCM {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) {
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
-            int i = 1;
-            int lcm=1;
-            while(num1!=i)
-
-            {
-                if (num1 % i == 0 && num2 % i == 0) {
-                    lcm *=i;
-                }
-            }
-        }
-    }
-}
---
+++ basic/src/prime1toN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class prime1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
