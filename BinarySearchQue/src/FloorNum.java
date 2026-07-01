248be2b0672c6d63c806aefb2cc99c79b8c59ec9
prachixmishrax <147589763+prachixmishrax@users.noreply.github.com>
2026-07-01 12:01:22 +0530

    Search floor of a given number in sorted array

---
+++ BinarySearchQue/src/FloorNum.java
@@ -0,0 +1,22 @@
+
+public class FloorNum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 9};
+        int target = 7;
+        System.out.println(floorNum(arr, target));
+    }
+
+    static int floorNum(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (target > arr[mid]) {
+                s = mid + 1;
+            } else if (target < arr[mid]) {
+                e = mid - 1;
+            } else return arr[mid];
+        }
+        return arr[e];
+    }
+}
