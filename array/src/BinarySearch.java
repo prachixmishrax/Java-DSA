a4322505fe015d5ef3719568dbcef5cf902d2008
prachixmishrax <147589763+prachixmishrax@users.noreply.github.com>
2026-07-01 11:49:49 +0530

    Binary Search

---
+++ array/src/BinarySearch.java
@@ -0,0 +1,20 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,7,9};
+        int target = 7;
+        System.out.println(binarySearch(arr,target));
+    }
+    static int binarySearch(int[] arr,int target){
+        int s = 0;
+        int e = arr.length-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(target>arr[mid]){
+                s= mid+1;
+            } else if (target<arr[mid]) {
+                e=mid-1;
+            }else return mid;
+        }
+        return -1;
+    }
+}
