0ea8bc811dc8df1be3f2f2dc0c16750818d25f8d
prachixmishrax <147589763+prachixmishrax@users.noreply.github.com>
2026-08-09 21:13:46 +0530

    revision of 2d array

--- array/src/MultiDimentionalArrayO.java
+++ array/src/MultiDimentionalArrayO.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class MultiDimentionalArrayO {
     public static void main(String[] args) {
         int[][] arr = new int[3][4];
+        Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 3 ; i++) {
             for (int j = 0; j < 4; j++) {
-
+                arr[i][j]=sc.nextInt();
             }
+        }
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
