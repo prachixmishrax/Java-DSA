2641defe0e793a89d3f80a8ba46fa05e529e6ab7
prachixmishrax <147589763+prachixmishrax@users.noreply.github.com>
2026-08-09 21:04:32 +0530

    count digit of a given number through string

---
+++ array/src/MultiDimentionalArrayO.java
@@ -0,0 +1,10 @@
+public class MultiDimentionalArrayO {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][4];
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 4; j++) {
+
+            }
+        }
+    }
+}
